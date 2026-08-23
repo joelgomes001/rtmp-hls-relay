9506b0cfd23f21684a8b802fcfb030135020f1c6
Joel Sohan Gomes <joel.s.gomes001@gmail.com>
2026-08-23 19:00:08 +0530

    Fix buffer stalls: remove -re and increase hls buffer window

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@ mkdir -p /tmp/hls
 nginx
 
 while true; do
-  ffmpeg -re -i "rtmp://139.59.48.129:1935/Mango/mango" \
+  ffmpeg -i "rtmp://139.59.48.129:1935/Mango/mango" \
     -c copy \
     -f hls \
-    -hls_time 2 \
-    -hls_list_size 5 \
+    -hls_time 4 \
+    -hls_list_size 10 \
     -hls_flags delete_segments \
     /tmp/hls/live.m3u8
   sleep 2
